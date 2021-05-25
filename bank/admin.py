4b281f02d944a2e6eaed5e42edd5d9abe743a187
bagas kara <karab4946@gmail.com>
2021-05-25 23:12:06 +0700

    admin optimize

--- bank/admin.py
+++ bank/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Accounts,Accounttransactions,Customers
 # Register your models here.
 
-admin.site.register((Accounts,Accounttransactions,Customers))
+admin.site.site_header = 'Bank Management Admin DASHBOARD'
+
+"SELECT id_customer, name FROM customer"
+class CustomersTable(admin.ModelAdmin):
+    list_display = ["id_customer","name",]
+    
+
+admin.site.register((Accounts,Accounttransactions))
+admin.site.register(Customers,CustomersTable)
